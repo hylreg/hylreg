518760c9e63f2c2bc3d174cf9f8529131b348b23
hylreg <hylreg@qq.com>
2025-01-01 23:07:40 +0800

    添加点云订阅

---
+++ Demo/MyROS/src/my_point_cloud/src/point_cloud.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("hello world my_point_cloud package\n");
+
+
+
+  return 0;
+}
---
+++ Demo/MyROS/src/my_point_cloud/src/point_cloud_pub.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+
+// #include "rclcpp/rclcpp.hpp"
+// #include "std_msgs/msg/string.hpp"
+// #include "sensor_msgs/msg/point_cloud2.hpp"
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("hello world my_point_cloud package\n");
+
+
+
+
+  return 0;
+}
